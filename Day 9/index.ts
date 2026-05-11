84f7c9554cbc85a1832f653497c2db8061992de7
varshiniragavan <varshiniragavan1@gmail.com>
2026-05-11 12:59:56 +0530

    day9 typescript basics

---
+++ Day 9/index.ts
@@ -0,0 +1,10 @@
+let name: string = "Varsh";
+name = "Tat";
+console.log(name);
+let res: number | string = 5;
+res = "Hello"
+console.log(res);
+function sum(a:number, b: number) : string{
+    return "Sum: " + (a+b);
+}
+console.log(sum(6,5));
